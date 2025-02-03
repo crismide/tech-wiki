40d4497ef3df618ed67f3a57987397fece438b77
Cris Milego <crissmilego@gmail.com>
2025-02-03 21:17:21 +0100

    tech wiki updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navigation from './components/Navigation'
+import Home from './assets/Home';
 
 function App() {
   const [activeTab, setActiveTab] = useState<string>('home');
 
   return (
-    <div className=''>
+    <div>
       <Navigation activeTab={activeTab} setActiveTab={setActiveTab}/>
-      {activeTab == 'home'?<h1>Home</h1>:<h1>Roadmap</h1>}
+      {activeTab == 'home'? <Home/> :<h1>Roadmap</h1>}
     </div>
     
   );
